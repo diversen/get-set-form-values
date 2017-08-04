7dcdddfc1e26819654a850ac981f04c092a388f2
Dennis Iversen <dennis.iversen@gmail.com>
2017-08-04 22:43:15 +0200

    add files

---
+++ dist/get-set-form-values.js
@@ -0,0 +1,121 @@
+/* Package: get-set-form-values. Version: 1.0.0. License: MIT. Author: dennis iversen. Homepage: https://github.com/diversen/get-set-form-values-js#readme   */ (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.getSetFormValues = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function getFormValues(formElement) {
+    var formElements = formElement.elements;
+    var formParams = {};
+    var i = 0;
+    var elem = null;
+    for (i = 0; i < formElements.length; i += 1) {
+        elem = formElements[i];
+        switch (elem.type) {
+            case 'submit':
+                break;
+            case 'radio':
+                if (elem.checked) {
+                    formParams[elem.name] = elem.value;
+                }
+                break;
+            case 'checkbox':
+                if (elem.checked) {
+                    if (!formParams[elem.name]) {
+                        formParams[elem.name] = [elem.value];
+                    } else {
+                        formParams[elem.name].push(elem.value);
+                    }
+                }
+                break;
+            case 'select-multiple':
+                var selectValues = getSelectValues(elem);
+                if (selectValues.length > 0) {
+                    formParams[elem.name] = selectValues;
+                }
+                break;
+            default:
+                if (elem.value !== "") {
+                    formParams[elem.name] = elem.value;
+                }
+        }
+    }
+    return formParams;
+}
+
+function setFormValues(formElement, values) {
+    var formElements = formElement.elements;
+    var formParams = {};
+    var i = 0;
+    var elem = null;
+    for (i = 0; i < formElements.length; i += 1) {
+        elem = formElements[i];
+        
+        if ( !(elem.name in values) ) {
+            continue;
+        }
+        
+        switch (elem.type) {
+            case 'submit':
+                break;
+            case 'radio':
+                if (values[elem.name] === elem.value) {
+                    elem.checked = true;
+                } else {
+                    elem.checked = false;
+                }
+                break;
+            case 'checkbox':
+                if (values[elem.name].indexOf(elem.value)) {
+                    elem.checked = true;
+                } else {
+                    elem.checked = false;
+                }
+                break;
+            case 'select-multiple':
+                if (values[elem.name]) {
+                    setSelectValues(elem, values[elem.name]);
+                }
+                break;
+            default:
+                if (values[elem.name]) {
+                    elem.value = values[elem.name];
+                }
+                
+        }
+    }
+}
+
+function setSelectValues(selectElem, values) {
+    var options = selectElem.options;
+    var opt;
+    
+    for (var i = 0, iLen = options.length; i < iLen; i++) {
+        opt = options[i];
+        if (values.indexOf(opt.value) > -1 ) {
+            opt.selected = true;
+        } else {
+            opt.selected = false;
+        }        
+    }
+}
+
+function getSelectValues(select) {
+    var result = [];
+    var options = select && select.options;
+    var opt;
+
+    for (var i = 0, iLen = options.length; i < iLen; i++) {
+        opt = options[i];
+
+        if (opt.selected) {
+            result.push(opt.value || opt.text);
+        }
+    }
+    return result;
+}
+
+function getSetFormValues () {
+    this.set = setFormValues;
+    this.get = getFormValues;
+}
+
+module.exports = getSetFormValues;
+
+},{}]},{},[1])(1)
+});
