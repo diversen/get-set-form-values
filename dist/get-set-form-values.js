be2b39023a0ff5ce4b1a20ee59b7cd0d49216f3f
Dennis Iversen <dennis.iversen@gmail.com>
2017-09-07 18:53:12 +0200

    auto commit

--- dist/get-set-form-values.js
+++ dist/get-set-form-values.js
@@ -1,5 +1,5 @@
-/* Package: get-set-form-values. Version: 2.0.0. License: MIT. Author: dennis iversen. Homepage: https://github.com/diversen/get-set-form-values#readme   */ (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.getSetFormValues = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-function getElemCountByName (name) {
+/* Package: get-set-form-values. Version: 3.0.0. License: MIT. Author: dennis iversen. Homepage: https://github.com/diversen/get-set-form-values#readme   */ (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.getSetFormValues = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function getElemCountByName(name) {
     var names = document.getElementsByName(name);
     return names.length;
 }
@@ -12,36 +12,22 @@ function getFormValues(formElement) {
     for (i = 0; i < formElements.length; i += 1) {
         elem = formElements[i];
         switch (elem.type) {
-            
+
             case 'submit':
                 break;
+
             case 'radio':
                 if (elem.checked) {
                     formParams[elem.name] = elem.value;
                 }
                 break;
+
             case 'checkbox':
-                
-                // Single checkbox
-                var numElems = getElemCountByName(elem.name);
-                if (numElems === 1) {
-                    if (elem.checked) {
-                        if (!formParams[elem.name]) {
-                            formParams[elem.name] = elem.value;
-                        }
-                    }
-                    break;
-                }
-                
-                // Multiple
                 if (elem.checked) {
-                    if (!formParams[elem.name]) {
-                        formParams[elem.name] = [elem.value];
-                    } else {
-                        formParams[elem.name].push(elem.value);
-                    }
+                    formParams[elem.name] = elem.value;
                 }
                 break;
+
             case 'select-multiple':
                 var selectValues = getSelectValues(elem);
                 if (selectValues.length > 0) {
@@ -64,11 +50,7 @@ function setFormValues(formElement, values) {
     var elem = null;
     for (i = 0; i < formElements.length; i += 1) {
         elem = formElements[i];
-        
-        if ( !(elem.name in values) ) {
-            continue;
-        }
-        
+
         switch (elem.type) {
             case 'submit':
                 break;
@@ -80,7 +62,7 @@ function setFormValues(formElement, values) {
                 }
                 break;
             case 'checkbox':
-                if (values[elem.name].indexOf(elem.value)) {
+                if (values[elem.name] === elem.value) {
                     elem.checked = true;
                 } else {
                     elem.checked = false;
@@ -95,7 +77,7 @@ function setFormValues(formElement, values) {
                 if (values[elem.name]) {
                     elem.value = values[elem.name];
                 }
-                
+
         }
     }
 }
@@ -103,14 +85,14 @@ function setFormValues(formElement, values) {
 function setSelectValues(selectElem, values) {
     var options = selectElem.options;
     var opt;
-    
+
     for (var i = 0, iLen = options.length; i < iLen; i++) {
         opt = options[i];
-        if (values.indexOf(opt.value) > -1 ) {
+        if (values.indexOf(opt.value) > -1) {
             opt.selected = true;
         } else {
             opt.selected = false;
-        }        
+        }
     }
 }
 
@@ -129,7 +111,7 @@ function getSelectValues(select) {
     return result;
 }
 
-function getSetFormValues () {
+function getSetFormValues() {
     this.set = setFormValues;
     this.get = getFormValues;
 }
